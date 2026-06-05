425aac3af3eeacba2fc9fbc7d7cccc02750ee05a
nikoG4 <nikoovelarrealg4@gmail.com>
2026-06-05 19:32:50 -0300

    fix: import sp unit in docs site demo page

--- docs-site/src/commonMain/kotlin/io/github/adaptivekt/site/SiteDemoPage.kt
+++ docs-site/src/commonMain/kotlin/io/github/adaptivekt/site/SiteDemoPage.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.github.adaptivekt.components.AdaptiveBadge
 import io.github.adaptivekt.components.AdaptiveBadgeTone
 import io.github.adaptivekt.components.AdaptiveButton
